aabc6c2f44111dfad0e60b9b2bae23c468861bf2
Namejess <jessy_drouin@protonmail.com>
2023-01-10 15:41:58 +0100

    🦀 First commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+//add the modules
+mod api; 
+mod models;
+mod repository;
+
+use actix_web::{web::Data, App, HttpServer};
+use api::user_api::{create_user};
+use repository::mongodb_repo::MongoRepo;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db = MongoRepo::init().await;
+    let db_data = Data::new(db);
+    HttpServer::new(move || {
+        App::new()
+            .app_data(db_data.clone())
+            .service(create_user)
+    })
+    .bind("localhost:8000")?
+    .run()
+    .await
+}
+
---
+++ src/repository/mongodb_repo.rs
@@ -0,0 +1,57 @@
+use std::env;
+extern crate dotenv;
+use dotenv::dotenv;
+
+use mongodb::{
+    bson::{extjson::de::Error, oid::ObjectId, doc},
+    results::{InsertOneResult},
+    Client, Collection,
+};
+
+use crate::models::user_model::User;
+
+pub struct MongoRepo {
+    col: Collection<User>,
+}
+
+impl MongoRepo {
+    pub async fn init() -> Self {
+        dotenv().ok();
+        let uri = match env::var("MONGOURI") {
+            Ok(v) => v.to_string(),
+            Err(_) => format!("Error loading env variable"),
+        };
+        let client = Client::with_uri_str(uri).await.unwrap();
+        let db = client.database("rustDB");
+        let col: Collection<User> = db.collection("User");
+        MongoRepo { col }
+    }
+
+    pub async fn create_user(&self, new_user: User) -> Result<InsertOneResult, Error> {
+        let new_doc = User {
+            id: None,
+            name: new_user.name,
+            location: new_user.location,
+            title: new_user.title,
+        };
+        let user = self
+            .col
+            .insert_one(new_doc, None)
+            .await
+            .ok()
+            .expect("Failed to insert document.");
+        Ok(user)
+        }
+
+    // pub async fn get_user(&self, id: &String) -> Result<User, Error> {
+    //     let obj_id = ObjectId::parse_str(id).unwrap();
+    //     let filter = doc! {"_id": obj_id};
+    //     let user_detail = self
+    //         .col
+    //         .find_one(filter, None)
+    //         .await
+    //         .ok()
+    //         .expect("Failed to execute find.");
+    //     Ok(user_detail.unwrap())
+    // }
+}
